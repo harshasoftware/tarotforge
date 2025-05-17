99759fd8e32633314cc0be6ef93d418b4d20ae24
Sri Harsha <sriharrsha@users.noreply.github.com>
2025-05-17 15:46:44 +0530

    Fix TarotLogo hover animation

--- src/components/ui/TarotLogo.tsx
+++ src/components/ui/TarotLogo.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface TarotLogoProps {
   className?: string;
 }
 
 const TarotLogo: React.FC<TarotLogoProps> = ({ className = "h-8 w-8" }) => {
+  const [isHovering, setIsHovering] = useState(false);
+  
   return (
-    <img 
-      src="/tarot-icon.svg" 
-      alt="Tarot Forge Logo" 
-      className={`transition-transform duration-700 hover:rotate-360 ${className}`} 
-    />
+    <div 
+      className="inline-block"
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <img 
+        src="/tarot-icon.svg" 
+        alt="Tarot Forge Logo" 
+        className={`transform transition-all duration-700 ${isHovering ? 'rotate-[360deg]' : 'rotate-0'} ${className}`}
+      />
+    </div>
   );
 };
 
