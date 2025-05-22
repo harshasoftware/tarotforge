fff9dec6ea8973879bc9a5b4300c0058079dd059
Sri Harsha <sriharrsha@users.noreply.github.com>
2025-05-23 03:58:10 +0530

    Enhance Video Player with Card Animations

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import * as Sentry from "@sentry/react";
 import setupLogRocketReact from 'logrocket-react';
 import LogRocket from 'logrocket';
 import { trackPageView } from './utils/analytics';
-import ScrollToTop from './ui/ScrollToTop';
+import ScrollToTop from '../ui/ScrollToTop';
 
 // Initialize LogRocket React plugin
 setupLogRocketReact(LogRocket);
