6ad27a29a5972cb57749d319f8c92806c1c40a82
Sri Harsha <sriharrsha@users.noreply.github.com>
2025-07-23 16:36:53 +0530

    fix: Update service worker revision for caching consistency
    - Updated the revision number for the precacheAndRoute configuration in sw.js to "0.u9k1v9vm9jo" to ensure the latest version of index.html is cached.
    - Adjusted the source mapping in sw.js.map to reflect the new file path, maintaining accurate references for debugging and performance tracking.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.a52vpjna8jg"
+    "revision": "0.u9k1v9vm9jo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
