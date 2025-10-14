d83b3a8bc76c3a426eed6f15898bcf658d2a0d06
Sri Harsha <sriharrsha@users.noreply.github.com>
2025-10-15 00:12:33 +0800

    fix: Set maximum file size to cache in service worker to 3 MB

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
         navigateFallback: 'index.html',
         skipWaiting: true,
         clientsClaim: true,
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3 MB
       },
       devOptions: {
         enabled: true,
